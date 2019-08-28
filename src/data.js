ee3312c354c3b28670d88032a5a0d6d6f9111e9e
Filadelfo Braz <filadelfo.braz@gmail.com>
2019-08-28 17:37:48 -0400

    data from public folder

---
+++ src/data.js
@@ -0,0 +1,37 @@
+const img1 = '/public/images/bw_ny.jpg'
+const img2 = '/public/images/bh_wc.jpg'
+const img3 = '/public/images/bsas_old_stuff.jpg'
+const img4 = '/public/images/capadocia_ballon.jpg'
+const img5 = '/public/images/gate_berlin.jpg'
+const img6 = '/public/images/lazy_rio.jpg'
+const img7 = '/public/images/love_rio.jpg'
+const img8 = '/public/images/luangPrabang_ms.jpg'
+const img9 = '/public/images/nj_gradient.jpg'
+const img10 = '/public/images/ny_jungle.jpg'
+const img11 = '/public/images/siempReap.jpg'
+const img12 = '/public/images/siemRiemsunset.jpg'
+const img13 = '/public/images/Spicy_Istambul.jpg'
+const img14 = '/public/images/sugar_loaf_rio.jpg'
+const img15 = '/public/images/traffic_nyc.jpg'
+const img16 = '/public/images/xing Argentina.jpg'
+
+
+//url, x, y, factor, z, scale
+export default [
+  [img1, 2, 1, 20, 0, 0.5],
+  [img2,  -2, -1, 15, -5, 0.5],
+  [img3,  2, 0, 10, -10, 0.5],
+  [img4,  1, 1, 10, -15, 0.5],
+  [img5,  3, -3, 10, -20, 0.5],
+  // [img6,  1, 1, 10, -2, 0.5],
+  // [img7,  1, 1, 10, -2, 0.5],
+  // [img8,  1, 1, 10, -2, 0.5],
+  // [img9,  1, 1, 10, -2, 0.5],
+  // [img10,  1, 1, 10, -2, 0.5],
+  // [img11,  1, 1, 10, -2, 0.5],
+  // [img12,  1, 1, 10, -2, 0.5],
+  // [img13,  1, 1, 10, -2, 0.5],
+  // [img14,  1, 1, 10, -2, 0.5],
+  // [img15,  1, 1, 10, -2, 0.5],
+  // [img16,  1, 1, 10, -2, 0.5],
+]
